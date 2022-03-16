3a31c398cf3da8c1cb89fd549dd25a0305fd55b1
jyyoun1022 <jyyoun1022@naver.com>
2022-03-16 17:25:26 +0900

    검색처리 구현체

--- src/main/java/part2/part2/Part2Application.java
+++ src/main/java/part2/part2/Part2Application.java
@@ -1,8 +1,12 @@
 package part2.part2;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import javax.persistence.EntityManager;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -11,5 +15,6 @@ public class Part2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Part2Application.class, args);
 	}
-
 }
+
+
